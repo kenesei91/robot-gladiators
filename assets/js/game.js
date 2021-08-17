3f4eeed78deecb9a46c6e09e433c6add68a75b24
Ken Ilo <keneilo91@yahoo.com>
2021-08-17 15:35:15 -0500

    functions, console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! Javascript is running!");
+function fight() {
+    var playerName = window.prompt("What is your robot's name?");
+    console.log(playerName);
+    console.log("This logs a string, good for leaving yourself a message");
+    console.log(10 + 10);
+    console.log("Our robot's name is " + playerName);
+    window.alert("This fight has begun!");
+}
+fight();
